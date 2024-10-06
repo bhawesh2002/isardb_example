730bde1dd1ba21071b49bb426d2147eb16d74c5b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-06 08:41:30 +0530

    Refactor recipe keys to separate file

---
+++ lib/utils/constants/recipie_keys.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+const String RECIPE_NAME = 'recipeName';
+
+const String RECIPE_DESCRIPTION = 'recipeDescription';
+
+const String RECIPE_CREATION_TIME = 'recipeCreationTime';
+
+const String RECIPE_IMAGE = 'recipeImage';
