dcadb082a9ef3d85c6edd88a39b0770d551af00b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-06 08:56:30 +0530

    Refactor ingredient keys to separate file

---
+++ lib/utils/constants/ingredient_keys.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+const String INGREDIENT_NAME = 'ingredientName';
+const String INGREDIENT_QUANTITY = 'ingredientQuantity';
+const String INGREDIENT_UNIT = 'ingredientUnit';
+const String INGREDIENT_IS_OPTIONAL = 'ingredientIsOptional';
