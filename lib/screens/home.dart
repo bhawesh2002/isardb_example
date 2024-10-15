1f41cd3b01aa96bc19f5665d26222d7fd6f9b9e9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-15 19:18:44 +0530

    Refactor Home widget to use themeProvider for managing the theme mode

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar_example/theme/theme_provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
     return Scaffold(
       body: const Center(
         child: Text("Home Page"),
       ),
-      bottomNavigationBar: bottomNavigationBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(themeProvider.notifier).changeTheme(
+              Theme.of(context).brightness == Brightness.dark
+                  ? ThemeMode.light
+                  : ThemeMode.dark);
+        },
+        child: Icon(
+            themeMode == ThemeMode.dark ? Icons.toggle_off : Icons.toggle_on),
+      ),
     );
   }
 }
