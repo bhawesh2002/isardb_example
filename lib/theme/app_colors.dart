d6724c3f930f7b95469cb9ab1a1f6d77b2b321cb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-15 08:00:26 +0530

    Add AppColors class for defining custom colors in the app

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+
+  static const Color turquoise500 = Color(0xff17cfb9);
+
+  ///Color used for the scaffold background in light theme
+  ///Hex: DDE6E6
+  static const Color mystic50 = Color(0xFFDDE6E6);
+
+  ///Color used for the scaffold background in dark theme
+  ///Hex: 161717
+  static const Color woodsmoke950 = Color(0xFF161717);
+}
