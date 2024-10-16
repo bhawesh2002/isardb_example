07bd48b2a911d9ff42e867241e4a1c8128d1464b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-16 14:30:30 +0530

    Add constant for storing current locale information

--- lib/utils/constants/shared_prefs_keys.dart
+++ lib/utils/constants/shared_prefs_keys.dart
@@ -2,3 +2,6 @@
 
 /// The key for the value that will store ThemeMode that the app is using
 const String THEME_MODE = 'themeMode';
+
+/// The key for storing a List which has two strings language code and country code for a locale
+const String CURRENT_LOCALE = 'currentLocale';
