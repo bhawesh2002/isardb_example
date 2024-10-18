7aa3944484dc6094410ac6abcc64972107eabc09
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-18 12:12:07 +0530

    remove unused import statements from locale_provider.dart

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar_example/utils/constants/shared_prefs_keys.dart';
