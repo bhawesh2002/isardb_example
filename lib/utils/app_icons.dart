d6bd33e028382d851fd21f487ae2a2b173335fb8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-18 18:03:35 +0530

    Add new SVG icons and update AppIcons class

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,11 @@
+class AppIcons {
+  static const String _iconsPath = 'assets/icons';
+  static const String home = '$_iconsPath/home.svg';
+  static const String search = '$_iconsPath/search.svg';
+  static const String heart = '$_iconsPath/heart.svg';
+  static const String settings = '$_iconsPath/settings.svg';
+  static const String share = '$_iconsPath/share-2.svg';
+  static const String bookmark = '$_iconsPath/bookmark.svg';
+  static const String arrowRight = '$_iconsPath/home.svg';
+  static const String arrowLeft = '$_iconsPath/arrow-left.svg';
+}
