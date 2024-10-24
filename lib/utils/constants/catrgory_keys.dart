3284341f5d90ddca62e1facc822d154b027abc00
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-10-24 07:24:46 +0530

    feat: add category keys

---
+++ lib/utils/constants/catrgory_keys.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+const String CATEGORY_NAME = 'categoryName';
+
+const String CATEGORY_IMG = 'categoryImage';
+
+const String CATEGORY_COLOR = 'categoryColor';
